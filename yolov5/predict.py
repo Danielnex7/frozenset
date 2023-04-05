6891cd17aa0bd01bf3523b8d9d667b22ea9ab6a5
danielnex7 <danielnex@live.ru>
2023-04-05 15:39:40 +0300

    Yolov5

--- yolov5/predict.py
+++ yolov5/predict.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 yolov5 = YOLO("yolov5/best.pt")
 # Запуск предсказания
-yolov5.predict("Examples/am3_1_frame004.jpg", save=True, save_txt=True)
+yolov5.predict("ssdlite/short_test/000009.jpg", save=True, save_txt=True)
